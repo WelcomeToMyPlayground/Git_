9be4a7d5bd1a0738742366baa4b610ed931e521d
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-06-28 14:09:16 +0900

    Add files via upload

---
+++ SelfStudyJava/chap04/src/sec01/중첩for예제2.java
@@ -0,0 +1,16 @@
+package sec02;
+
+public class ÁßÃ¸for¿¹Á¦2 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 5; j >= i; j--) {
+				System.out.print("*");
+
+			}
+			System.out.println();
+		}
+	}
+
+}
