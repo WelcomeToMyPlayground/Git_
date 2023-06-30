dab6ebb6fa7d87fb0c210917c423c9fc7c684dfc
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-06-30 15:40:59 +0900

    Add files via upload

---
+++ SelfStudyJava/chap05/src/sec01/문자열비교.java
@@ -0,0 +1,34 @@
+package sec01;
+
+import java.util.Scanner;
+
+public class 문자열비교 {
+
+	public static void main(String[] args) {
+		String var1 = "홍길동"; // 변수 사용 안해서 경고 
+		String var2 = "홍길동";
+
+		Scanner s = new Scanner(System.in);
+		String a = s.next();
+		String b = s.next();
+		s.close();
+		if (a == b) {
+			System.out.println("두 데이터 참조가 같아요");
+		} else {
+			System.out.println("두 데이터 참조가 달라요");
+
+		}
+		String var3 = new String("홍길동");
+		String var4 = new String("홍길동");
+		if (var3 == var4) {
+			System.out.println("두 데이터 참조가 같아요");
+		} else {
+			System.out.println("두 데이터 참조가 달라요");
+		}
+		
+		String t;
+		
+		
+	}
+
+}
