f7b176ee692456789113e3651a253b1a77d3f53e
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-07-03 18:44:17 +0900

    Update 배열1.java

--- SelfStudyJava/chap05/src/sec02/배열1.java
+++ SelfStudyJava/chap05/src/sec02/배열1.java
@@ -1,9 +1,9 @@
 package sec02;
 
-public class 배열1 {
+public class 諛곗뿴1 {
 
 	public static void main(String[] args) {
-		int[] score = { 50, 80, 90 }; /* 배열의 시작은 0부터  */
+		int[] score = { 50, 80, 90 }; 
 		for (int i = 0; i <= 2; i++) {
 			System.out.println(score[i]);
 		}
