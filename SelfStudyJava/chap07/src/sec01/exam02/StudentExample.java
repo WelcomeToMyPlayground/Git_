9a94f15b32a0799eb9ab72e92efb75e217b804e6
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-07-11 21:52:36 +0900

    Update StudentExample.java

--- SelfStudyJava/chap07/src/sec01/exam02/StudentExample.java
+++ SelfStudyJava/chap07/src/sec01/exam02/StudentExample.java
@@ -3,7 +3,7 @@ package sec01.exam02;
 public class StudentExample {
 
 	public static void main(String[] args) {
-		Student student = new Student("±èÅÂ±Õ","123456-789012",20231234);
+		Student student = new Student("í™ê¸¸ë™","123456-789012",20231234);
 		System.out.println(student.name);
 		System.out.println(student.ssn);
 		System.out.println(student.studentNo);
