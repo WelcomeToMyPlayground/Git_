f5ce02f6576753e1b61aff3d5077a87a949bd083
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-07-13 22:15:02 +0900

    Update InstanceofExample.java

--- SelfStudyJava/chap07/src/sec02/exam06/InstanceofExample.java
+++ SelfStudyJava/chap07/src/sec02/exam06/InstanceofExample.java
@@ -12,7 +12,9 @@ public class InstanceofExample {
 
 	public static void method1(Parent parent) {
 
-		if (parent instanceof Child) { // 강제 타입 변환 전 instanceof 연산자로 검사하는 것이 좋음 (but instanceof를 사용하는 것은 객체지향 언어인 자바의 캡슐화의 의미를 무색하게 하므로 가급적 지양하는 것을 권장)
+		if (parent instanceof Child) { // 강제 타입 변환 전 instanceof 연산자로 검사하는 것이 좋음 (but instanceof를 사용하는 것은 
+						//객체지향 언어인 자바의 캡슐화의 의미를 무색하게 하므로 가급적 지양하는 것을 권장)
+						// instanceof 대신 abstract를 권장
 			Child child = (Child) parent; // child를 parent에 상속시켰기 때문에 강제형변환 가능
 			System.out.println("강제 변환 성공");
 		} else {
